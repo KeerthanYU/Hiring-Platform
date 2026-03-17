e0ac8d5a14fb0a72ed174e8554d8e260bb1169ac
KeerthanYU <keerthanyu88@gmail.com>
2026-03-17 18:54:02 +0530

    The Deployment process

--- backend/src/index.js
+++ backend/src/index.js
@@ -37,7 +37,7 @@ const app = express();
 // =======================
 app.use(
     cors({
-        origin: true,
+        origin: process.env.GOOGLE_CALLBACK_URL,
         credentials: true,
     })
 );
