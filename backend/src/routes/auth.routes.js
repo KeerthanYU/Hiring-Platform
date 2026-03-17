5219edc6ae285843cd3f5ff032ba902df15ab6e9
KeerthanYU <keerthanyu88@gmail.com>
2026-03-17 19:44:18 +0530

    Deployment Stage

--- backend/src/routes/auth.routes.js
+++ backend/src/routes/auth.routes.js
@@ -1,5 +1,4 @@
 import express from "express";
-import passport from "passport";
 import { register, login, me } from "../controllers/auth.controller.js";
 import auth from "../middleware/auth.middleware.js";
 
@@ -9,19 +8,5 @@ router.post("/register", register);
 router.post("/login", login);
 router.get("/me", auth, me);
 
-// Google Login
-router.get(
-    "/google",
-    passport.authenticate("google", { scope: ["profile", "email"] })
-);
-
-// Google Callback
-router.get(
-    "/google/callback",
-    passport.authenticate("google", { session: false }),
-    (req, res) => {
-        res.redirect("https://hiringplatform-keerthanyus-projects.vercel.app/dashboard");
-    }
-);
 
 export default router;
